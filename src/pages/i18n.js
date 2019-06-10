6400e3ee8215789d49bac3b15162314ecf71c4a5
daviddyess <david.dyess@gmail.com>
2019-06-09 20:36:59 -0600

    Styling improvements in i18n page

--- src/pages/i18n.js
+++ src/pages/i18n.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import { I18n } from 'react-i18next';
 import { withI18next } from 'gatsby-plugin-i18next';
+import { Alert, Card } from 'react-bootstrap';
 
 import Layout from 'components/Layout';
 import SEO from 'components/SEO';
@@ -18,13 +19,25 @@ const i18n = () => (
           {t('i18n_heading')}
         </Heading>
         <h3>{t('i18n_stmt')}</h3>
-        <div className="jumbotron">
-          {Object.keys(locale).map(key => (
-            <pre key={key}>
-              &quot;{key}&quot; : &quot;{locale[key]}&quot;
-            </pre>
-          ))}
-        </div>
+        <Alert variant="info">{t('i18n_info')}</Alert>
+        <Card border="info">
+          <Card.Header>
+            File: locale/en/locale.json :&nbsp;
+            <a href="https://github.com/gusta-project/website/tree/master/src/locale/en/locale.json">
+              https://github.com/gusta-project/website/tree/master/src/locale/en/locale.json
+            </a>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>
+              {Object.keys(locale).map(key => (
+                <pre key={key}>
+                  &quot;{key}&quot; : &quot;{locale[key]}&quot;
+                </pre>
+              ))}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>{t('i18n_footer')}</Card.Footer>
+        </Card>
       </Layout>
     )}
   </I18n>
