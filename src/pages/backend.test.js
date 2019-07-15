4c6102b7a1b627227af8e9a65ce5d2bc5135387b
ayan4m1 <andrew@bulletlogic.com>
2019-07-15 19:52:54 -0400

    mock header and footer in all page tests, add mocks to components

--- src/pages/backend.test.js
+++ src/pages/backend.test.js
@@ -3,6 +3,9 @@ import renderer from 'react-test-renderer';
 
 import Backend from './backend';
 
+jest.mock('components/Header', () => require('utils').mockComponent('Header'));
+jest.mock('components/Footer', () => require('utils').mockComponent('Footer'));
+
 describe('BackendPage', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<Backend />).toJSON();
--- src/pages/index.test.js
+++ src/pages/index.test.js
@@ -3,6 +3,9 @@ import renderer from 'react-test-renderer';
 
 import IndexPage from './index';
 
+jest.mock('components/Header', () => require('utils').mockComponent('Header'));
+jest.mock('components/Footer', () => require('utils').mockComponent('Footer'));
+
 describe('HomePage', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<IndexPage />).toJSON();
