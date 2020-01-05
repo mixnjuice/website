2ccf9d65bb9f8df4574231ac24db3fb2ec295131
ayan4m1 <andrew@bulletlogic.com>
2020-01-04 22:43:13 -0500

    mock Date for footer tests

--- src/components/Footer.test.js
+++ src/components/Footer.test.js
@@ -3,6 +3,8 @@ import renderer from 'react-test-renderer';
 
 import Footer from './Footer';
 
+Date.now = jest.fn(() => new Date(2020, 0, 1, 12));
+
 describe('Footer', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<Footer siteTitle="Footer Test" />).toJSON();
